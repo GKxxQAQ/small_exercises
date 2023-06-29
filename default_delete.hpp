6782163d6f3708bd5ad16746bd91de90d2235eb4
GKxxQAQ <2863746329@qq.com>
2023-06-30 00:33:51 +0800

    namespace detail

--- default_delete.hpp
+++ default_delete.hpp
@@ -13,21 +13,21 @@
 
 namespace gkxx {
 
-namespace detail::default_delete {
+namespace detail {
 
   template <typename T>
   concept complete_type = !std::is_void_v<T> && requires { sizeof(T); };
 
-} // namespace detail::default_delete
+} // namespace detail
 
 template <typename T>
 struct default_delete {
   constexpr default_delete() noexcept = default;
   template <typename U>
-    requires std::convertible<U *, T *>
+    requires std::convertible_to<U *, T *>
   CXX23_CONSTEXPR default_delete(const default_delete<U> &) noexcept {}
   CXX23_CONSTEXPR void operator()(T *ptr) const noexcept(noexcept(delete ptr)) {
-    static_assert(detail::default_delete::complete_type<T>,
+    static_assert(detail::complete_type<T>,
                   "delete a pointer to incomplete type");
     delete ptr;
   }
@@ -37,13 +37,13 @@ template <typename T>
 struct default_delete<T[]> {
   constexpr default_delete() noexcept = default;
   template <typename U>
-    requires std::convertible<U (*)[], T (*)[]>
+    requires std::convertible_to<U (*)[], T (*)[]>
   CXX23_CONSTEXPR default_delete(const default_delete<U[]> &) noexcept {}
   template <typename U>
-    requires std::convertible<U (*)[], T (*)[]>
+    requires std::convertible_to<U (*)[], T (*)[]>
   CXX23_CONSTEXPR void operator()(U *ptr) const
       noexcept(noexcept(delete[] ptr)) {
-    static_assert(detail::default_delete::complete_type<T>,
+    static_assert(detail::complete_type<T>,
                   "delete a pointer to incomplete type");
     delete[] ptr;
   }
