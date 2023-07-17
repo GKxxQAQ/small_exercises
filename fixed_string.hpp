ec819c8ba0d63e0a577d4818aad0731fdb9f00ff
GKxxQAQ <2863746329@qq.com>
2023-07-17 16:27:42 +0800

    some functions must be consteval

--- fixed_string.hpp
+++ fixed_string.hpp
@@ -13,28 +13,28 @@ namespace gkxx {
 /// @tparam N The length of the string
 template <std::size_t N>
 struct fixed_string {
-  constexpr fixed_string(const char (&str)[N + 1]) {
+  consteval fixed_string(const char (&str)[N + 1]) {
     std::copy_n(str, N + 1, data);
   }
   auto operator<=>(const fixed_string &) const = default;
   bool operator==(const fixed_string &) const = default;
-  constexpr std::string_view as_sv() const noexcept {
+  constexpr std::string_view to_string_view() const noexcept {
     return {data, N};
   }
-  constexpr std::string as_string() const noexcept {
-    return std::string(as_sv());
+  constexpr std::string to_string() const noexcept {
+    return std::string(to_string_view());
   }
-  constexpr auto operator[](std::size_t i) const noexcept {
+  consteval auto operator[](std::size_t i) const noexcept {
     return data[i];
   }
   template <std::size_t Begin, std::size_t End>
-  constexpr fixed_string<End - Begin> slice() const noexcept {
+  consteval fixed_string<End - Begin> slice() const noexcept {
     char copy[End - Begin + 1];
     std::copy(data + Begin, data + End, copy);
     copy[End - Begin] = '\0';
     return copy;
   }
-  static constexpr auto size() noexcept {
+  static consteval auto size() noexcept {
     return N;
   }
   char data[N + 1]{};
@@ -45,13 +45,13 @@ fixed_string(const char (&)[N]) -> fixed_string<N - 1>;
 
 template <std::size_t N, std::size_t M>
   requires(N != M)
-inline constexpr bool operator==(const fixed_string<N> &,
+inline consteval bool operator==(const fixed_string<N> &,
                                  const fixed_string<M> &) noexcept {
   return false;
 }
 
 template <std::size_t N, std::size_t M>
-inline constexpr auto operator+(const fixed_string<N> &lhs,
+inline consteval auto operator+(const fixed_string<N> &lhs,
                                 const fixed_string<M> &rhs) noexcept {
   char data[N + M + 1];
   std::copy_n(lhs.data, lhs.size(), data);
